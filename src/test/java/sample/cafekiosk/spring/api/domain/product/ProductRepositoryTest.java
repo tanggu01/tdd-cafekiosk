63c7bb41ff1799b561d03a3deb6ecaa80f088119
KIM TAEHYUN <tanggu01@connect.hku.hk>
2025-04-11 15:36:30 +0800

    Persistence layer test

---
+++ src/test/java/sample/cafekiosk/spring/api/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,67 @@
+package sample.cafekiosk.spring.api.domain.product;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+import static sample.cafekiosk.spring.api.domain.product.ProductSellingStatus.*;
+
+//@SpringBootTest // 스프링에서 통합테스트를 위해 제공하는 annotation. 스프링 서버를 띄워서 테스트 가능
+@DataJpaTest // SpringBootTest 보다 가볍다. JPA 관련 빈만 주입
+@ActiveProfiles("test")
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @DisplayName("원하는 판매상태를 가진 상품들을 조회한다.")
+    void findAllBySellingStatusIn() {
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        // then
+        Assertions.assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus") // 검증하고자 하는 필드만 추출
+                .containsExactlyInAnyOrder( // 순서 상관없이 확인
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
+
+    }
+
+
+}
