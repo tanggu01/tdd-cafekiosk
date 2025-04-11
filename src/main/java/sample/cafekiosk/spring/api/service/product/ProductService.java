57a1d4d5ddc7c742ff4715b32709b16c94c31e2a
KIM TAEHYUN <tanggu01@connect.hku.hk>
2025-04-11 15:36:17 +0800

    Section 5: Spring Test

---
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -0,0 +1,26 @@
+package sample.cafekiosk.spring.api.service.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.domain.product.Product;
+import sample.cafekiosk.spring.api.domain.product.ProductRepository;
+import sample.cafekiosk.spring.api.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.api.domain.product.response.ProductResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+        return products.stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+}
